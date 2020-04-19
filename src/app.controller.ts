45cd57ffd6b61abcdd933ec8d6152c7363f2c05c
cojack <xcojack@gmail.com>
2020-04-19 23:10:58 +0200

    wip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import {Register} from "./discovery/register.decorator";
+import { Expose } from './discovery/expose.decorator';
+import { ZookeeperOptions } from './discovery-zookeeper/zookeeper-config.interface';
 
 @Controller()
-@Register()
+@Expose<ZookeeperOptions>({
+  path: 'qwe',
+})
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
   getHello(): string {
